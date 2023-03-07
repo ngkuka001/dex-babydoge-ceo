2f2d5b4bc77f3c985b222713f5373828bdca0348
ngkuka001 <ngkuka001@gmail.com>
2023-03-07 14:21:04 +0700

    update router

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,7 +2,6 @@ import PublicLayout from 'components/Layout/Public';
 import EarnPage from 'pages/earn';
 import HomePage from 'pages/home';
 import NftPage from 'pages/nft';
-import SwapPage from 'pages/trade';
 import TradePage from 'pages/trade';
 
 export const ROUTE_URLS = {
@@ -70,14 +69,6 @@ const routes = [
     name: PAGE_KEYS.WIN,
     path: ROUTE_URLS.WIN,
     component: NftPage,
-    layout: PublicLayout,
-    isPrivate: false,
-    index: false,
-  },
-  {
-    name: PAGE_KEYS.SWAP,
-    path: ROUTE_URLS.SWAP,
-    component: SwapPage,
     layout: PublicLayout,
     isPrivate: false,
     index: false,
